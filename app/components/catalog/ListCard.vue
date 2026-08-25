05f498af604752df31ca6caa947045241ab9a629
Alexander Morugin <nobilis@bk.ru>
2026-08-25 19:41:58 +0300

    catalog icons

--- app/components/catalog/ListCard.vue
+++ app/components/catalog/ListCard.vue
@@ -7,6 +7,39 @@
       <span class="catalogListCard__subtitle">{{ item.subtitle }}</span>
     </div>
 
+    <IconStep
+      v-if="item.route === '/step'"
+      class="catalogListCard__clip catalogListCard__clip_step"
+    />
+    <IconTile
+      v-if="item.route === '/tile'"
+      class="catalogListCard__clip catalogListCard__clip_tile"
+    />
+    <IconSill
+      v-if="item.route === '/sill'"
+      class="catalogListCard__clip catalogListCard__clip_sill"
+    />
+    <IconInterior
+      v-if="item.route === '/interior'"
+      class="catalogListCard__clip catalogListCard__clip_interior"
+    />
+    <IconSouvenir
+      v-if="item.route === '/souvenir'"
+      class="catalogListCard__clip catalogListCard__clip_souvenir"
+    />
+    <IconMonument
+      v-if="item.route === '/monument'"
+      class="catalogListCard__clip catalogListCard__clip_monument"
+    />
+    <IconBaluster
+      v-if="item.route === '/baluster'"
+      class="catalogListCard__clip catalogListCard__clip_baluster"
+    />
+    <IconWorktop
+      v-if="item.route === '/worktop'"
+      class="catalogListCard__clip catalogListCard__clip_worktop"
+    />
+
     <div class="catalogListCard__markBlock">
       <div class="catalogListCard__mark">
         <IconArrowIos class="catalogListCard__markIcon" />
@@ -25,14 +58,16 @@ const props = defineProps<{
 
 <style lang="scss" scoped>
 .catalogListCard {
+  position: relative;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
-  min-height: 300px;
-  background: $white-mask-five;
+  min-height: 250px;
+  background: $white-one;
   border-radius: $br-m;
   padding: 1rem;
   transition: 0.2s ease;
+  overflow: hidden;
 
   &:hover {
     background: $white-mask-four;
@@ -44,8 +79,9 @@ const props = defineProps<{
   }
 
   &__title {
+    color: $black-one;
     min-height: 75px;
-    border-bottom: 1px solid $white-mask-three;
+    border-bottom: 1px solid $black-mask-two;
 
     @media (max-width: 1024px) {
       min-height: 62px;
@@ -54,7 +90,7 @@ const props = defineProps<{
 
   &__subtitle {
     font-size: 14px;
-    color: $white-mask-two;
+    color: $black-mask-two;
     overflow: hidden;
     display: -webkit-box;
     -webkit-line-clamp: 2;
@@ -82,6 +118,69 @@ const props = defineProps<{
     width: 16px;
     height: 16px;
     fill: $black-one;
+  }
+
+  &__clip {
+    position: absolute;
+
+    fill: $black-one;
+
+    &_step {
+      bottom: -20px;
+      left: -20px;
+      width: 200px;
+      height: 200px;
+    }
+
+    &_tile {
+      bottom: -20px;
+      left: 0;
+      width: 150px;
+      height: 150px;
+      transform: rotate(157deg);
+    }
+
+    &_baluster {
+      bottom: -20px;
+      left: 20px;
+      width: 120px;
+      height: 120px;
+    }
+
+    &_sill {
+      bottom: 0;
+      left: 20px;
+      width: 150px;
+      height: 150px;
+    }
+
+    &_worktop {
+      bottom: -30px;
+      left: 50px;
+      width: 150px;
+      height: 150px;
+    }
+
+    &_interior {
+      bottom: -30px;
+      left: 50px;
+      width: 150px;
+      height: 150px;
+    }
+
+    &_souvenir {
+      bottom: 0;
+      left: 50px;
+      width: 100px;
+      height: 100px;
+    }
+
+    &_monument {
+      bottom: -30px;
+      left: 50px;
+      width: 150px;
+      height: 150px;
+    }
   }
 }
 
---
+++ app/components/icon/Step.vue
@@ -0,0 +1,14 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24px"
+    height="24px"
+    viewBox="0 -960 960 960"
+    fill="none"
+  >
+    <!-- <path
+      d="M100-220v-60h233.27v-230h233.27v-230H860v60H626.73v230H393.46v230H100Z"
+    /> -->
+    <path d="M80-200v-80h240v-240h240v-240h320v80H640v240H400v240H80Z" />
+  </svg>
+</template>
