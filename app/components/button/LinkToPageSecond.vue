30a25f3677a67fd381ed39329d1d9afcaf6c9073
Alexander Morugin <nobilis@bk.ru>
2026-09-26 19:54:25 +0300

    fix catalog

--- app/components/button/LinkToPageSecond.vue
+++ app/components/button/LinkToPageSecond.vue
@@ -17,38 +17,27 @@ const props = defineProps<{
   display: flex;
   justify-content: space-between;
   align-items: center;
-  // height: 36px;
-  // border-bottom: 1px solid $white-mask-four;
+  height: 36px;
+  border-radius: $br-xxs;
   border: 1px solid $white-mask-four;
-  padding-left: 5px;
-  padding-right: 5px;
-  // transition: 0.05s ease;
-
-  // &:last-child {
-  //   border-bottom: 1px solid transparent;
-  // }
+  padding-left: 10px;
+  padding-right: 10px;
 
   &:hover {
     background: $white-mask-four;
   }
 
   &__title {
-    font-size: 14px;
-    line-height: 2;
-    // transition: 0.2s ease;
+    letter-spacing: 1px;
   }
 
   &__icon {
-    fill: $white-mask-two;
+    fill: $deep-blue-three;
     width: 14px;
     height: 14px;
-    // transition: 0.2s ease;
   }
 }
 
-.buttonLinkToPageSecond:hover .buttonLinkToPageSecond__title {
-  color: $orange-four;
-}
 .buttonLinkToPageSecond:hover .buttonLinkToPageSecond__icon {
   fill: $orange-four;
 }
--- app/components/catalog/MainBlock.vue
+++ app/components/catalog/MainBlock.vue
@@ -3,65 +3,18 @@
     <TitleBlockMain title="Каталог продукции" />
 
     <div class="catalogMainBlock__container">
-      <div class="catalogMainBlock__block">
-        <ButtonLinkToPageSecond
-          v-for="item in stepList"
-          :key="item.id"
-          :path="item.route"
-          :title="item.title"
-        />
-      </div>
-
-      <div class="catalogMainBlock__block">
-        <ButtonLinkToPageSecond
-          v-for="item in plateList"
-          :key="item.id"
-          :path="item.route"
-          :title="item.title"
-        />
-      </div>
-
-      <!-- <div class="catalogMainBlock__block"> -->
       <ButtonLinkToPageSecond
-        path="/catalog/stepping-slabs"
-        title="Шаговые плиты"
+        v-for="item in catalog"
+        :key="item.id"
+        :path="`/catalog${item.route}`"
+        :title="item.titleSmall"
       />
-      <!-- </div> -->
-
-      <!-- <div class="catalogMainBlock__block"> -->
-      <ButtonLinkToPageSecond path="/catalog/baluster" title="Балюстрады" />
-      <!-- </div> -->
-
-      <!-- <div class="catalogMainBlock__block"> -->
-      <ButtonLinkToPageSecond path="/catalog/sill" title="Подоконники" />
-      <!-- </div> -->
-
-      <!-- <div class="catalogMainBlock__block"> -->
-      <ButtonLinkToPageSecond path="/catalog/worktop" title="Столешницы" />
-      <!-- </div> -->
-
-      <!-- <div class="catalogMainBlock__block"> -->
-      <ButtonLinkToPageSecond path="/catalog/monument" title="Памятники" />
-      <!-- </div> -->
-
-      <!-- <div class="catalogMainBlock__block"> -->
-      <ButtonLinkToPageSecond path="/catalog/vazon" title="Вазоны" />
-      <!-- </div> -->
-
-      <!-- <div class="catalogMainBlock__block"> -->
-      <ButtonLinkToPageSecond path="/catalog/barbecue" title="Барбекю" />
-      <!-- </div> -->
-
-      <!-- <div class="catalogMainBlock__block"> -->
-      <ButtonLinkToPageSecond path="/catalog/souvenir" title="Сувениры" />
-      <!-- </div> -->
     </div>
   </section>
 </template>
 
 <script setup lang="ts">
-import { plateList } from "~/mock/plate/plate-list";
-import { stepList } from "~/mock/step/step-list";
+import { catalog } from "~/mock/catalog";
 </script>
 
 <style lang="scss" scoped>
@@ -69,26 +22,32 @@ import { stepList } from "~/mock/step/step-list";
   margin-top: 100px;
 
   &__container {
-    display: flex;
-    flex-direction: column;
-    gap: 10px;
-    // height: 400px;
-    // border-radius: 20px;
+    display: grid;
+    grid-template-columns: repeat(5, 1fr);
+    column-gap: 20px;
+    row-gap: 10px;
     margin-top: 10px;
-    border: 1px solid $white-mask-three;
+    background: $gradient-white-four;
+    padding: 30px 20px;
+    border-radius: $br-xxs;
+
+    @media (max-width: 1280px) {
+      grid-template-columns: repeat(4, 1fr);
+    }
+
+    @media (max-width: 1024px) {
+      grid-template-columns: repeat(3, 1fr);
+    }
+
+    @media (max-width: 767px) {
+      grid-template-columns: repeat(2, 1fr);
+      background: none;
+      padding: 0;
+    }
 
     @media (max-width: 576px) {
-      border: 1px solid transparent;
+      grid-template-columns: 1fr;
     }
-  }
-
-  &__block {
-    display: flex;
-    flex-direction: column;
-    gap: 10px;
-    // border-radius: 20px;
-    // border: 1px solid $white-mask-four;
-    // padding: 10px;
   }
 }
 </style>
